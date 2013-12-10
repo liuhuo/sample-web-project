9b2ea0242eaec33db5617908dd67ecc3c75afcec
Liu Huo <huoliu@gmail.com>
2013-12-10 10:15:19 +0800

    add highcharts basics

--- src/main/java/org/liuhuo/spring/controller/DummyUserController.java
+++ src/main/java/org/liuhuo/spring/controller/DummyUserController.java
@@ -11,9 +11,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 import org.liuhuo.spring.model.DummyUser;
 import org.liuhuo.spring.dao.DummyUserDao;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 @Controller
 public class DummyUserController {
@@ -34,9 +44,36 @@ public class DummyUserController {
     }
 
     @RequestMapping(value = "/alluser", method = RequestMethod.GET)
-    public @ResponseBody DummyUser alluser() {
-	DummyUser user = dummyUserDao.selectAll().get(0);
-	System.out.println(user);
-	return user;
+    public @ResponseBody List<DummyUser>  alluser() {
+	List<DummyUser> users = dummyUserDao.selectAll();
+	System.out.println(users);
+	return users;
+    }
+
+    @RequestMapping(value = "/poll")
+    public @ResponseBody String poll() throws Exception {
+	CloseableHttpClient httpclient = HttpClients.createDefault();
+	try {
+	    HttpGet  httpget = new HttpGet("https://www.okcoin.com/api/ticker.do?symbol=ltc_cny");
+	    ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+		public String handleResponse(final HttpResponse response) throws ClientProtocolException, IOException {
+                    int status = response.getStatusLine().getStatusCode();
+                    if (status >= 200 && status < 300) {
+                        HttpEntity entity = response.getEntity();
+                        return entity != null ? EntityUtils.toString(entity) : null;
+                    } else {
+                        throw new ClientProtocolException("Unexpected response status: " + status);
+                    }
+                }
+	    };
+	    String responseBody = httpclient.execute(httpget, responseHandler);
+            System.out.println("----------------------------------------");
+            System.out.println(responseBody);
+            System.out.println("----------------------------------------");
+	    return responseBody;
+	}
+	finally {
+	    httpclient.close();
+	}
     }
 }
--- src/main/webapp/WEB-INF/templates/all.ftl
+++ src/main/webapp/WEB-INF/templates/all.ftl
@@ -1,9 +1,11 @@
 <html>
 <head>
+  <meta http-equiv="Content-type" content="text/html; charset=UTF-8">
   <title>dummy user list example</title>
   <script type="text/javascript" src="resources/js/jquery-2.0.3.js"></script>
+  <script type="text/javascript" src="resources/js/highcharts.js"></script>
   <script type="text/javascript" src="resources/js/jquery.handsontable.full.js"></script>
-  <link rel="stylesheet" media="screen" href="resources/css/jquery.handsontable.css">
+  <link rel="stylesheet" media="screen" href="resources/css/jquery.handsontable.full.css">
 
   <script type="text/javascript" src="resources/js/dummyuser.js">
 
@@ -13,7 +15,7 @@
 <body>
 <div id="header">
 <h2>
-    FreeMarker Spring MVC Example
+    FreeMarker Spring MVC Example示例
 </h2>
 </div>
 
@@ -35,9 +37,14 @@
     </#list>
   </table>
 
-  <button id="button1" type="button">Click Me!</button>
+  <button id="button1" type="button">show me显示</button>
 
   <div id="dataTable"></div>
+
+  <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto">
+  </div>
+
+  <div id = "ltc"></div>
 </div>
 </body>
 </html>
